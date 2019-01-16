530b518ef1e7ff9bbc062ae4cb9cbca9465919c7
WarriorKozak <gm.tupota.valik@gmail.com>
2019-01-16 14:35:44 +0200

    --fix issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms'
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
+//component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetStartedComponent } from './get-started/get-started.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
--- src/app/get-started/get-started.component.ts
+++ src/app/get-started/get-started.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-get-started',
   templateUrl: './get-started.component.html',
   styleUrls: ['./get-started.component.css']
 })
-export class GetStartedComponent implements OnInit {
+export class GetStartedComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
