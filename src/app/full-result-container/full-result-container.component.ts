716f52c778529a6e35cfa90a3df8600ce73ea86b
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-21 17:34:10 +0200

    Add components full-result-container and result-item

--- src/app/full-result-container/full-result-container.component.ts
+++ src/app/full-result-container/full-result-container.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-full-result-container',
   templateUrl: './full-result-container.component.html',
   styleUrls: ['./full-result-container.component.css']
 })
-export class FullResultContainerComponent implements OnInit {
+export class FullResultContainerComponent {
 
-  constructor() { }
+  @Input() resultItem: any;
 
-  ngOnInit() {
+  constructor() {
+    this.resultItem = {
+      surname: "TestSurname",
+      name: "TestName",
+      secondName: "TestSecondName",
+      email: "TestEmail",
+      answers: 2,
+      maxCountAnswers: 5,
+      results: [{
+        url: "../assets/models/good_template_1.svg",
+        comment: "some commentar about template",
+        mark: true,
+        name: "Good template #1"
+      },{
+        url: "../../assets/models/Colorful-5.jpg",
+        comment: "like!",
+        mark: true,
+        name: "Bad template #2"
+      },{
+        url: "../../assets/models/bad_template_3.svg",
+        comment: "so bad...",
+        mark: false,
+        name: "Bad template #3"
+      }]
+    }
   }
 
 }
--- src/app/result-item/result-item.component.ts
+++ src/app/result-item/result-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-result-item',
   templateUrl: './result-item.component.html',
   styleUrls: ['./result-item.component.css']
 })
-export class ResultItemComponent implements OnInit {
 
-  constructor() { }
+export class ResultItemComponent {
 
-  ngOnInit() {
+  @Input() index: number;
+  @Input() item: any;
+
+  constructor() {
+    // this.item = {
+    //   url: "../assets/models/good_template_1.svg",
+    //   comment: "some commentar about template",
+    //   mark: true,
+    //   name: "Good template #1"
+    // }
   }
 
 }
