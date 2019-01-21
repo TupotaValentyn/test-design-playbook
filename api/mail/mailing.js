db6c1309a2210da1651b0941b0cef4d43c721414
CreativeMan17 <kvska@meta.ua>
2019-01-21 16:23:56 +0200

    the best edits

--- api/mail/mailing.js
+++ api/mail/mailing.js
@@ -2,7 +2,7 @@ const Mailgun = require('mailgun-js');
 
 const API_KEY = '905b6d8793aa95dae52d5c62e5931962-2d27312c-d3893eba';
 const DOMAIN = 'sandboxb98f54e9299c4c9cb51f00c87e5c4804.mailgun.org';
-const FROM_WHO = 'postmaster@sandboxb98f54e9299c4c9cb51f00c87e5c4804.mailgun.org';//'test-design-playbook@protonmail.com';
+const FROM_WHO = 'postmaster@sandboxb98f54e9299c4c9cb51f00c87e5c4804.mailgun.org';
 
 module.exports = function (email, subject, text) {
     const mailgun = new Mailgun({apiKey: API_KEY, domain: DOMAIN});
@@ -12,11 +12,11 @@ module.exports = function (email, subject, text) {
         subject: subject,
         text: text
     };
-    mailgun.messages().send(data, function (error, body) {
+    mailgun.messages().send(data, (error, body) => {
         if (error) {
-            throw error ;
+            throw error;
         } else {
             return body;
         }
-      });
+    });
 };
