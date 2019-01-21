b2d0823b743f2bc6886be9de94c67805bd3af1cf
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-21 15:59:14 +0200

    Remove unused components

--- src/app/admin-navbar/admin-navbar.component.ts
+++ src/app/admin-navbar/admin-navbar.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-navbar',
@@ -9,12 +6,4 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./admin-navbar.component.css']
 })
 export class AdminNavbarComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
 }
