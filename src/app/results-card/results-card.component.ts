7c344a761597c29deee41e93665fcfed30af264d
WarriorKozak <gm.tupota.valik@gmail.com>
2019-01-21 17:49:54 +0200

    add card, output through loop

--- src/app/results-card/results-card.component.ts
+++ src/app/results-card/results-card.component.ts
@@ -10,20 +10,20 @@ export class ResultsCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.childData)
-    this.results = this.childData['0'].results
+    this.results = this.childData.results
     this.fullname = `
-      ${ this.childData['0'].surname } ${ this.childData['0'].firstname } ${ this.childData['0'].secondname }
+      ${ this.childData.surname } 
+      ${ this.childData.firstname } 
+      ${ this.childData.secondname }
       `;
-    this.comment = this.childData['0'].comment
-    this.answer = this.childData['0'].answers
-    this.maxAnswer = this.childData['0'].maxAnswers
-    console.log(this.fullname)
-    console.log(this.comment)
+    this.comment = this.childData.comment
+    this.answer = this.childData.answers
+    this.maxAnswer = this.childData.maxAnswers
 
   }
 
   @Input() childData: any;
+  @Input() index: any;
   results: any;
   fullname: string;
   comment: string;
--- src/app/results-component/results-component.component.ts
+++ src/app/results-component/results-component.component.ts
@@ -29,6 +29,54 @@ export class ResultsComponentComponent implements OnInit {
           name: 'Some full-name'
         }
       ]
+    },
+    {
+      commentExternal: 'external - comment',
+      surname: 'bondar',
+      firstname: 'oleksit',
+      secondname: 'volodymirivich',
+      answers: 4,
+      maxAnswers: 5,
+      results: [
+        {
+          url: 'img.jpg',
+          commentInto: 'some comment',
+          mark: false,
+          name: 'Some full-name'
+        }
+      ]
+    },
+    {
+      commentExternal: 'external - comment',
+      surname: 'tupota',
+      firstname: 'valintin',
+      secondname: 'uriyovich',
+      answers: 2,
+      maxAnswers: 5,
+      results: [
+        {
+          url: 'img.jpg',
+          commentInto: 'some comment',
+          mark: false,
+          name: 'Some full-name'
+        }
+      ]
+    },
+    {
+      commentExternal: 'external - comment',
+      surname: 'tupota',
+      firstname: 'valintin',
+      secondname: 'uriyovich',
+      answers: 2,
+      maxAnswers: 5,
+      results: [
+        {
+          url: 'img.jpg',
+          commentInto: 'some comment',
+          mark: false,
+          name: 'Some full-name'
+        }
+      ]
     }
   ]
 
