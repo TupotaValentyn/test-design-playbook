13aa8d2a3f5168b2a9e69f426a1695a513eb096e
InterLink Intern <intern@intern-pc.interlink.office>
2019-01-22 17:53:05 +0200

    Add link tab

--- src/app/admin-panel-link-info/admin-panel-link-info.component.ts
+++ src/app/admin-panel-link-info/admin-panel-link-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-admin-panel-link-info',
@@ -11,5 +11,7 @@ export class AdminPanelLinkInfoComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  @Input() usersDataItem: any;
 
 }
--- src/app/admin-panel-link/admin-panel-link.component.ts
+++ src/app/admin-panel-link/admin-panel-link.component.ts
@@ -12,4 +12,32 @@ export class AdminPanelLinkComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  //TEST ARR OF DATE
+  usersDateInforArray = [
+    {
+      name: 'Mirek',
+      surname: 'Sokol',
+      secondName: 'Smth',
+      email: 'smthverylognemail@ukr.net',
+      created: new Date(),
+      expires: new Date()
+    },
+    {
+      name: 'Anton',
+      surname: 'Olexanrovich',
+      secondName: 'Olegiv',
+      email: 'smthverylognemail@ukr.net',
+      created: new Date(),
+      expires: new Date()
+    },
+    {
+      name: 'Boris',
+      surname: 'Dmitrovich',
+      secondName: 'Vorobeev',
+      email: 'smthverylognemail@ukr.net',
+      created: new Date(),
+      expires: new Date()
+    }
+  ];
 }
