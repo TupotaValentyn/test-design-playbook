bc838981194ae50092ba0abdd7b4373af388aa99
WarriorKozak <gm.tupota.valik@gmail.com>
2019-01-22 18:26:30 +0200

    add navbarpanel to results-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 // component
 import { MainContainerComponent } from './main-container/main-container.component';
 import { GetStartedComponent } from './get-started/get-started.component';
-import { ThxPageComponent } from './thx-page/thx-page.component'
+import { ThxPageComponent } from './thx-page/thx-page.component';
+import { ResultsPageComponent } from './results-page/results-page.component'
 
 const routes: Routes = [
   { path: 'main', component: MainContainerComponent },
   { path: '', component: GetStartedComponent },
-  { path: 'finish', component: ThxPageComponent }
+  { path: 'finish', component: ThxPageComponent },
+  { path: 'admin/main', component: ResultsPageComponent }
 ];
 
 @NgModule({
