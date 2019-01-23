cddd8207ad56788ac32ec77803977683cca55065
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-23 10:45:10 +0200

    Change styles for full-result-container

--- src/app/full-result-container/full-result-container.component.ts
+++ src/app/full-result-container/full-result-container.component.ts
@@ -19,34 +19,19 @@ export class FullResultContainerComponent {
       maxCountAnswers: 5,
       results: [{
         url: "../assets/models/good_template_1.svg",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
-          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
-          "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
-          "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
-          "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
-          "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        comment: "",
         mark: true,
         answer: true,
         name: "Good template #1"
       },{
         url: "../../assets/models/Colorful-5.jpg",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
-          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
-          "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
-          "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
-          "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
-          "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        comment: "",
         mark: true,
         answer: false,
         name: "Bad template #2"
       },{
         url: "../../assets/models/bad_template_3.svg",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
-          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
-          "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
-          "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
-          "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
-          "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        comment: "",
         mark: false,
         answer: false,
         name: "Bad template #3"
