51f7ac81eefe343b251cc372053d3d1d5f2b2fbc
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-23 11:50:53 +0200

    Add components test-result-table and test-result-row

---
+++ src/app/test-result-row/test-result-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-result-row',
+  templateUrl: './test-result-row.component.html',
+  styleUrls: ['./test-result-row.component.css']
+})
+export class TestResultRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/test-result-table/test-result-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-result-table',
+  templateUrl: './test-result-table.component.html',
+  styleUrls: ['./test-result-table.component.css']
+})
+export class TestResultTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
