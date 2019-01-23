d4e7c9db63908e147bab6972aa7c16a8a44f4e7d
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-23 16:54:44 +0200

    Edit markup and styles for components test-result-table and test-result-row

--- src/app/test-result-row/test-result-row.component.ts
+++ src/app/test-result-row/test-result-row.component.ts
@@ -1,25 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test-result-row',
   templateUrl: './test-result-row.component.html',
   styleUrls: ['./test-result-row.component.css']
 })
-export class TestResultRowComponent implements OnInit {
+export class TestResultRowComponent {
 
-  @Input() item: any;
   @Input() index: number;
+  @Input() item: any;
 
-  constructor() {
-    this.index = 1;
-    this.item = {
-      url: "",
-      mark: true,
-      comment: "Good!"
-    };
-  }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
--- src/app/test-result-table/test-result-table.component.ts
+++ src/app/test-result-table/test-result-table.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test-result-table',
   templateUrl: './test-result-table.component.html',
   styleUrls: ['./test-result-table.component.css']
 })
-export class TestResultTableComponent implements OnInit {
+export class TestResultTableComponent {
 
   @Input() countCheckedElements: number;
   @Input() maxCountCheckedElements: number;
@@ -17,13 +17,29 @@ export class TestResultTableComponent implements OnInit {
     this.maxCountCheckedElements = 5;
 
     this.models = [{
-      url: "",
+      url: "../../assets/models/Colorful-1.jpg",
+      mark: false,
+      comment: "Bad template"
+    },{
+      url: "../../assets/models/Colorful-5.jpg",
+      mark: false,
+      comment: "Bad colors"
+    },{
+      url: "../../assets/models/bad_template_3.svg",
+      mark: false,
+      comment: "so bad..."
+    },{
+      url: "../../assets/models/bad_template_4.svg",
+      mark: false,
+      comment: ":(",
+    },{
+      url: "../../assets/models/bad_template_5.svg",
       mark: true,
-      comment: ""
+      comment: "I really liked it!"
+    },{
+      url: "../../assets/models/bad_template_6.svg",
+      mark: true,
+      comment: "It's good too."
     }];
   }
-
-  ngOnInit() {
-  }
-
 }
