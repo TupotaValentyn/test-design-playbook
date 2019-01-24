c2eb7844e0b7f71ecc372bdce02e96feba292b7f
polishchuk <thevivalley@gmail.com>
2019-01-24 12:50:07 +0200

    add field _id to model

--- src/app/models/model.ts
+++ src/app/models/model.ts
@@ -2,6 +2,7 @@ export class Model {
   static IMAGE_PREFIX: string = '../../assets';
   static DEFAULT_IMAGE_URL: string = '../../assets/empty_img.png';
 
+  _id: string;
   url: string;
   answer: boolean;
   name: string;
