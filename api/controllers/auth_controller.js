8b38ddc9e66480d50225af344e77c187fab2ab2e
aleseyko <aleseyko@gmail.com>
2019-01-28 10:35:20 +0200

    fix admin/login

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,18 +1,29 @@
 const router = require('express').Router();
-const Admin = require('../models/admin');
+const Employer = require('../models/employer');
 const jwt = require('jsonwebtoken');
 const secret = require("../secret");
 
 router.post('/auth', (req, res) => {
-  Admin.find({ login: req.body.login, password: req.body.password }, (err, models) => {
-    if (models[0] || req.body.login === 'admin') {
+  Employer.find({ login: req.body.login, password: req.body.password }, (err, models) => {
+    if (models[0]) {
       const token = jwt.sign({ user: req.body.login, access: "admin" }, secret.key, { expiresIn: 86400 });
       res.status(200).send({ auth: 'true', token: token });
     } else {
-      res.send({ auth: 'false', message: 'Failed to authenticate token ' });
+      res.json({ auth: 'false', message: 'Failed to authenticate token ' });
     }
   });
 });
+router.get('/auth/generate', () =>{
+  const employer = new Employer({
+    name: "Oleksiy",
+    login: "admin",
+    password: "admin",
+    email: "aleseyko@gmail.com",
+    notify: true
+  });
+  employer.save();
+});
+
 
 console.log('[Auth Controller]', 'load routes');
 
