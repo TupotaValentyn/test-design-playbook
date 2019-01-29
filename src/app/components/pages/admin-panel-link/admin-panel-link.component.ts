e33d5acd651917dd5b8c33f0279fe17eed2f0c37
pass <Kvska@meta.ua>
2019-01-29 09:58:23 +0200

    form validation

--- src/app/components/pages/admin-panel-link/admin-panel-link.component.ts
+++ src/app/components/pages/admin-panel-link/admin-panel-link.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { DataSourceService } from '../../shared/service/data-source.service';
 import { Applicant } from '../../shared/models/applicant';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-admin-panel-link',
   templateUrl: './admin-panel-link.component.html',
   styleUrls: ['./admin-panel-link.component.css']
 })
-export class AdminPanelLinkComponent implements OnInit{
+export class AdminPanelLinkComponent implements OnInit {
 
   constructor (private dataSource: DataSourceService) { }
 
@@ -27,17 +29,26 @@ export class AdminPanelLinkComponent implements OnInit{
       });
   }
 
+  onSubmit(form: NgForm) {
+    this.getLink(
+      form.value.email,
+      form.value.firstName,
+      form.value.surname,
+      form.value.secondName
+    );
+  }
+
   getLink(email, name, surname, secondname) {
     this.dataSource.createLinkForUser(email, name, surname, secondname)
       .subscribe((data: any) => {
         console.log(data);
-        if(data.token) {
+        if (data.token) {
           console.log(data.token);
           this.token = `/invite/${data.token}`;
           this.link = `http://localhost:4200${this.token}`;
           this.updateUsersDataList();
         } else {
-          alert('don\'t have permission')
+          alert('don\'t have permission');
         }
       });
   }
@@ -46,8 +57,8 @@ export class AdminPanelLinkComponent implements OnInit{
     const newLink = 'http://localhost:4200' + link;
     this.dataSource.sendMailWithLink(email, name, surname, secondname, link)
       .subscribe((data: any) => {
-      console.log(data)
-    })
+      console.log(data);
+    });
 }
 
   copyLink(target) {
