c0aeaa23ca91c18fc6ef0f5c173ffa2f31ebd8f2
aleseyko <aleseyko@gmail.com>
2019-01-31 12:27:25 +0200

    also refactor

--- src/app/components/pages/results-page/results-card/results-card.component.ts
+++ src/app/components/pages/results-page/results-card/results-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Result } from '../../../shared/models/result';
 import { DataSourceService } from '../../../shared/service/data-source.service';
 @Component({
