85671da119e4c4962831e86c25d56778dc2e9f2b
aleseyko <aleseyko@gmail.com>
2019-01-31 12:07:28 +0200

    add results delete

--- src/app/components/pages/results-page/results-card/results-card.component.ts
+++ src/app/components/pages/results-page/results-card/results-card.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Result } from '../../../shared/models/result';
-
+import { DataSourceService } from '../../../shared/service/data-source.service';
 @Component({
   selector: 'app-results-card',
   templateUrl: './results-card.component.html',
@@ -8,10 +8,19 @@ import { Result } from '../../../shared/models/result';
 })
 export class ResultsCardComponent implements OnInit {
 
-  constructor() { }
+  @Output() onDelete = new EventEmitter();
+
+  constructor(private dataSource: DataSourceService) {  }
 
   ngOnInit() {
 
+  }
+
+  deleteResult(token) {
+    this.dataSource.deleteResult(token)
+      .subscribe(() =>{
+        this.onDelete.emit();
+      });
   }
 
   @Input() result_item: Result;
