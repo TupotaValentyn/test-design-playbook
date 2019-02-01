9c6ac361a76497a0fc49a2e043372a63aba850b9
Mirek Sokol <beztormoza@ukr.net>
2019-02-01 10:41:51 +0200

    added modal

---
+++ src/app/components/pages/main-container/help-instruction-modal/help-instruction-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-instruction-modal',
+  templateUrl: './help-instruction-modal.component.html',
+  styleUrls: ['./help-instruction-modal.component.css']
+})
+export class HelpInstructionModalComponent {}
