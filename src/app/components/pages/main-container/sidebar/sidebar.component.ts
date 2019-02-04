539b70b2ddc2e3dcabf14f671213af41fda34ff8
InterLink Intern <beztormoza@ukr.net>
2019-02-04 12:18:06 +0200

    fixed bugs and added scroll buttons in test-container on image

--- src/app/components/pages/main-container/sidebar/sidebar.component.ts
+++ src/app/components/pages/main-container/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {SolvedModel} from '../../../shared/models/solved-model';
 
 @Component({
@@ -6,21 +6,15 @@ import {SolvedModel} from '../../../shared/models/solved-model';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
   @Input() models: Array<SolvedModel>;
   @Output() onSelect = new EventEmitter<SolvedModel>();
 
   @Input() currentSelectedModel: SolvedModel;
 
-  selectedModel: SolvedModel;
-
-  ngOnInit() {
-    this.selectedModel = this.models[0];
-  }
-
   onSelectElement(model) {
-    this.selectedModel = model;
+    this.currentSelectedModel = model;
     this.onSelect.emit(model);
   }
 
