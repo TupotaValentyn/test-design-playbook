3e62ba015786e313047a007deae59527f169c53a
polishchuk <thevivalley@gmail.com>
2019-02-06 17:25:55 +0200

    update database anyway script

--- api/startup/database_checker.js
+++ api/startup/database_checker.js
@@ -7,12 +7,12 @@ const path = require('path');
 module.exports = () => {
   DataCreator.findOne({ first: true })
     .then((docs) => {
-      if (!docs) {
-        const dc = new DataCreator({
-          first: true,
-          time: new Date()
-        });
-        dc.save();
+      // if (!docs) {
+        // const dc = new DataCreator({
+        //   first: true,
+        //   time: new Date()
+        // });
+        // dc.save();
         fs.readFile(path.resolve(__dirname + './../layouts/backup.json'), {encoding: 'UTF-8'}, (err, data) => {
           if (err) throw err;
           const promises = [];
@@ -42,7 +42,7 @@ module.exports = () => {
               console.error(err);
             })
         });
-      }
+      // }
     });
 };
 
